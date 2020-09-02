e70a4e3a8cf40b4b0e9a972860e5feba3b56bbf6
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-02 23:19:45 +0200

    Compress the given string (count how many times char repeated continously)

--- 02.Algorithmic Strategies/05_compress_str.rb
+++ 02.Algorithmic Strategies/05_compress_str.rb
@@ -2,6 +2,20 @@
 # The method should return a new str where streaks of consecutive characters are compressed.
 # For example "aaabbc" is compressed to "3a2bc".
 
+def compress_str(str)
+    compressed_str = ""
+    c = 0
+    (1..str.length).each do |i|
+        c += 1
+        if str[i-1] == str[i]
+            next
+        end
+        compressed_str += c.to_s if c > 1 
+        compressed_str += str[i-1]
+        c = 0
+    end
+    compressed_str
+end
 
 p compress_str("aaabbc")        # => "3a2bc"
 p compress_str("xxyyyyzz")      # => "2x4y2z"
