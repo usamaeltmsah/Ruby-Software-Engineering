2ad80efe02be68bcdacb72123250084df19ebd88
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-02 17:17:32 +0200

    Solve the problem using inject method

--- 01.Advanced Methods/05_max_inject.rb
+++ 01.Advanced Methods/05_max_inject.rb
@@ -5,5 +5,18 @@ def max_inject(*arr)
     [*arr].max
 end
 
+def max_inject2(*arr)
+    arr.inject do |max, el|
+        if el > max
+            el
+        else
+            max
+        end
+    end
+end
+
 p max_inject(1, -4, 0, 7, 5)  # => 7
 p max_inject(30, 28, 18)      # => 30
+puts "----"
+p max_inject(1, -4, 0, 7, 100)  # => 100
+p max_inject(30, 50, 18)      # => 50
