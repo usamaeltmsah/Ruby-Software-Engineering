2a7cd1d3743610871ae403538796f5af706dcdbb
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-02 23:04:43 +0200

    Find the peaks elements in the array

--- 02.Algorithmic Strategies/04_peak_finder.rb
+++ 02.Algorithmic Strategies/04_peak_finder.rb
@@ -3,6 +3,20 @@
 # An element is considered a "peak" if it is greater than both it's left and right neighbor.
 # The first or last element of the array is considered a "peak" if it is greater than it's one neighbor.
 
+def peak_finder(arr)
+    peaks = []
+    l = arr.length
+    (0...l).each do |i|
+        if i == 0
+            peaks << arr[i] if arr[i] > arr [i+1]
+        elsif i == l - 1
+            peaks << arr[i] if arr[i] > arr [i-1]
+        else
+            peaks << arr[i] if arr[i] > arr[i-1] && arr[i] > arr[i+1]
+        end
+    end
+    peaks
+end
 
 p peak_finder([1, 3, 5, 4])         # => [5]
 p peak_finder([4, 2, 3, 6, 10])     # => [4, 10]
