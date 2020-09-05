e4eea9fbfb9dc30ea1d645032763001a98a491b2
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-05 21:21:55 +0200

    Check elements of the 2 arrays are same (order not necessary)

--- 07_Debugging_Exercise_2/lib/problems.rb
+++ 07_Debugging_Exercise_2/lib/problems.rb
@@ -24,9 +24,14 @@ def dupe_indices(arr)
     hash = Hash.new {|h,k| h[k] = Array.new }
     arr.each { |el| count[el] += 1}
     #debugger
-    arr.each_with_index do |el, i|
-        hash[el] << i if count[el] > 1
-    end
+    arr.each_with_index { |el, i| hash[el] << i if count[el] > 1 }
     hash
-    
+end
+
+def ana_array(arr1, arr2)
+    arr1_includes_arr2_elements = arr1.all? { |el| arr2.include?(el) }
+    arr2_includes_arr1_elements = arr2.all? { |el2| arr1.include?(el2) }
+    return true if arr1_includes_arr2_elements && arr2_includes_arr1_elements
+
+    return false
 end
