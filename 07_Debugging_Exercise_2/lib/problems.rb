9fcc9b4647cf5b13736c8e801c758fee2d8abfef
Usama Fouad <usamaeltmsah@gmail.com>
2020-09-05 19:39:50 +0200

    Get the largest prime factor for a number

--- 07_Debugging_Exercise_2/lib/problems.rb
+++ 07_Debugging_Exercise_2/lib/problems.rb
@@ -1,3 +1,11 @@
 # Run `bundle exec rspec` and satisy the specs.
 # You should implement your methods in this file.
 # Feel free to use the debugger when you get stuck.
+require "byebug"
+def largest_prime_factor(num)
+    require 'prime'
+    num.downto(2) do |factor|
+        #debugger
+        return factor if num % factor == 0 && factor.prime?
+    end 
+end
