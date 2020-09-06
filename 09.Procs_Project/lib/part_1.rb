c01133e24a95cb940e1e9d0ddbc483db92f97c9d
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-07 00:48:31 +0200

    Create my own select function

--- 09.Procs_Project/lib/part_1.rb
+++ 09.Procs_Project/lib/part_1.rb
@@ -3,3 +3,9 @@ def my_map(arr, &prc)
     arr.each { |el| new_arr << prc.call(el)}
     new_arr
 end
+
+def my_select(arr, &prc)
+    new_arr = []
+    arr.each { |el| new_arr << el if true == prc.call(el)}
+    new_arr
+end
