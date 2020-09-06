dd0d5cd4ffb2d3fd8d6e0672115dd75a44515b1c
Usama Fouad <usamaeltmsah@gmail.com>
2020-09-06 19:54:05 +0200

    No valid url

--- 08.Blocks_Project/lib/part_2.rb
+++ 08.Blocks_Project/lib/part_2.rb
@@ -1,3 +1,8 @@
 def all_words_capitalized?(words)
     words.all? { |w| w == w.capitalize}
 end
+
+def no_valid_url?(urls)
+    dom = ['.com', '.net', '.io', '.org']
+    urls.none? { |url| url.end_with?(dom[0]) || url.end_with?(dom[1]) || url.end_with?(dom[2]) || url.end_with?(dom[3]) }
+end
