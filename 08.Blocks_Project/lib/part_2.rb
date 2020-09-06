284a494a258f1918081b58bae04120488a799610
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-06 22:04:03 +0200

    No valid url

--- 08.Blocks_Project/lib/part_2.rb
+++ 08.Blocks_Project/lib/part_2.rb
@@ -3,6 +3,8 @@ def all_words_capitalized?(words)
 end
 
 def no_valid_url?(urls)
-    dom = ['.com', '.net', '.io', '.org']
-    urls.none? { |url| url.end_with?(dom[0]) || url.end_with?(dom[1]) || url.end_with?(dom[2]) || url.end_with?(dom[3]) }
+    doms = ['.com', '.net', '.io', '.org']
+    urls.none? do |url|
+        doms.any? { |dom| url.end_with?(dom)}
+    end
 end
