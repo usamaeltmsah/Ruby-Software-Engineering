fc526e1dd69466562c79c70ddfb7539b2da41b00
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-07 00:50:47 +0200

    Create my own count function

--- 09.Procs_Project/lib/part_1.rb
+++ 09.Procs_Project/lib/part_1.rb
@@ -9,3 +9,9 @@ def my_select(arr, &prc)
     arr.each { |el| new_arr << el if true == prc.call(el)}
     new_arr
 end
+
+def my_count(arr, &prc)
+    count = 0
+    arr.each { |el| count += 1 if true == prc.call(el)}
+    count
+end
