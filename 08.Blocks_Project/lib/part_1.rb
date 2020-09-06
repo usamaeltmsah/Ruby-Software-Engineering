c75d3aa5e85dd358960458e7f8a6029e038f9bfa
Usama Fouad <usamaeltmsah@gmail.com>
2020-09-06 19:39:45 +0200

    Aba Translate (Add 'b' after every vowel)

--- 08.Blocks_Project/lib/part_1.rb
+++ 08.Blocks_Project/lib/part_1.rb
@@ -9,3 +9,13 @@ end
 def count_positive_subarrays(arr)
     arr.count { |sub| sub.sum > 0}
 end
+
+def aba_translate(str)
+    vowels = "aeiou"
+    new_str = ""
+    str.each_char do |ch|
+        new_str += ch
+        new_str += "b" + ch if vowels.include?(ch)
+    end
+    new_str
+end
