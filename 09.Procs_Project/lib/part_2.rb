8c72b473e90ff0780831470fb0e2c1006d07da0f
Usama Fouad <usamaeltmsah@gmail.com>
2020-09-07 01:13:30 +0200

    Make alternating mapper

--- 09.Procs_Project/lib/part_2.rb
+++ 09.Procs_Project/lib/part_2.rb
@@ -21,3 +21,15 @@ def and_selector(arr, prc1, prc2)
     arr.each { |el| new_arr << el if prc1.call(el) && prc2.call(el)}
     new_arr
 end
+
+def alternating_mapper(arr, prc1, prc2)
+    new_arr = []
+    arr.each_with_index do |el, i|
+        if i % 2 == 0
+            new_arr << prc1.call(el)
+        else
+            new_arr << prc2.call(el)
+        end
+    end
+    new_arr
+end
