aee780df753c276c0ab47499177ecd193678e29c
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-07 01:04:34 +0200

    Make changes on sentence words

--- 09.Procs_Project/lib/part_2.rb
+++ 09.Procs_Project/lib/part_2.rb
@@ -1,3 +1,10 @@
 def reverser(str, &prc)
     prc.call(str.reverse)
 end
+
+def word_changer(sent, &prc)
+    words = sent.split
+    new_words = []
+    words.each { |w| new_words << prc.call(w) }
+    new_words.join(" ")
+end
