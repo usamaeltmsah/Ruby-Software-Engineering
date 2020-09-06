ac00875b26b4a38f15a6af960e2f2a205b1690c5
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-07 01:07:21 +0200

    Get value from the greater proc value

--- 09.Procs_Project/lib/part_2.rb
+++ 09.Procs_Project/lib/part_2.rb
@@ -8,3 +8,10 @@ def word_changer(sent, &prc)
     words.each { |w| new_words << prc.call(w) }
     new_words.join(" ")
 end
+
+def greater_proc_value(num, prc1, prc2)
+    res1 = prc1.call(num)
+    res2 = prc2.call(num)
+
+    return [res1, res2].max
+end
