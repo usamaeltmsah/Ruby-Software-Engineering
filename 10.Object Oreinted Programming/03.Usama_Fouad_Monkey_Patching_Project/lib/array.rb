7499686ca2f3c07fc1f2c27a3988945d81736f04
Usama Fouad <usamaeltmsah@gmail.com>
2020-09-07 23:44:06 +0200

    Get the median of an array

--- 10.Object Oreinted Programming/03.Usama_Fouad_Monkey_Patching_Project/lib/array.rb
+++ 10.Object Oreinted Programming/03.Usama_Fouad_Monkey_Patching_Project/lib/array.rb
@@ -7,4 +7,16 @@ class Array
   def average
     self.sum / (1.0 * self.length) if self.length > 0
   end
+
+  def median
+    len = self.length
+    sorted = self.sort
+    if len > 0
+        if len.odd?
+            return sorted[len/2]
+        else
+            return [sorted[(len/2)-1], sorted[len/2]].average
+        end
+    end
+  end
 end
