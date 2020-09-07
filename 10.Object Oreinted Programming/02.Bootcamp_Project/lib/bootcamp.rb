e7d5ee4e29f2cd1666b213f6420a9f9ead3e3b18
Usama Fouad <usamaeltmsah@gmail.com>
2020-09-07 22:00:52 +0200

    Create the constructor

--- 10.Object Oreinted Programming/02.Bootcamp_Project/lib/bootcamp.rb
+++ 10.Object Oreinted Programming/02.Bootcamp_Project/lib/bootcamp.rb
@@ -1,3 +1,11 @@
 class Bootcamp
-  
+    def initialize(name, slogan, student_capacity)
+        @name = name
+        @slogan = slogan
+        @student_capacity = student_capacity
+        @teachers = @students = []
+        @grades = Hash.new {|h,k| h[k] = [] }
+    end
+
+
 end
