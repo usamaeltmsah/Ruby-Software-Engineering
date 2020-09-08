4303fcb23c0fd0bb9ed413dfd4af2b50d315ab22
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-08 22:15:01 +0200

    Create the constuctor of the ckass

--- 10.Object Oreinted Programming/05.Hangman_Project/lib/hangman.rb
+++ 10.Object Oreinted Programming/05.Hangman_Project/lib/hangman.rb
@@ -4,4 +4,11 @@ class Hangman
   def self.random_word
     DICTIONARY.sample
   end
+
+  def initialize
+    @secret_word = Hangman.random_word
+    @guess_word = Array.new(@secret_word.length, '_')
+    @attempted_chars = []
+    @remaining_incorrect_guesses = 5
+  end
 end
