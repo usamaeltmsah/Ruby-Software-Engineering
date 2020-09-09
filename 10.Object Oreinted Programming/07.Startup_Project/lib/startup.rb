82e266c6c75e231764697465467c04cb51d5ab5d
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-09 23:48:54 +0200

    Constructor and Getters for Startup class

--- 10.Object Oreinted Programming/07.Startup_Project/lib/startup.rb
+++ 10.Object Oreinted Programming/07.Startup_Project/lib/startup.rb
@@ -1,5 +1,11 @@
 require "employee"
 
 class Startup
-
+    attr_reader :name, :funding, :salaries, :employees
+    def initialize(name, funding, salaries)
+        @name = name
+        @funding = funding
+        @salaries = salaries
+        @employees = []
+    end
 end
