c867d71bc68ea41ae35e6b2028c51fad1a5c9558
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-09 13:22:20 +0200

    Create constructor for Hotel class

--- 10.Object Oreinted Programming/06.Hotel_Project/lib/hotel.rb
+++ 10.Object Oreinted Programming/06.Hotel_Project/lib/hotel.rb
@@ -1,5 +1,12 @@
 require_relative "room"
 
 class Hotel
-  
+  def initialize(name, names_hash)
+    @name = name
+    @rooms = {}
+    names_hash.keys.each do |k| 
+        room = Room.new(names_hash[k])
+        @rooms[k] = room
+    end
+  end
 end
