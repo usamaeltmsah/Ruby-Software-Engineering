406b9e85a6df1907f0613628bdbee189b9f61795
Usama Fouad <usamaeltmsah@gmail.com>
2020-09-10 03:30:37 +0200

    Print exact and near match between 2 pegs

--- 10.Object Oreinted Programming/08.Mastermind_Project/lib/mastermind.rb
+++ 10.Object Oreinted Programming/08.Mastermind_Project/lib/mastermind.rb
@@ -4,4 +4,9 @@ class Mastermind
     def initialize(len)
         @secret_code = Code.random(len)
     end
+
+    def print_matches(code)
+        puts code.num_exact_matches(@secret_code)
+        puts code.num_near_matches(@secret_code)
+    end
 end
