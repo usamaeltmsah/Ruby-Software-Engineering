4b587934ac268c699319d0c2c3c5f240a28bf961
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-10 23:19:47 +0200

    Select from hash using procs

--- 12.Recap_Exercise_1/lib/problems.rb
+++ 12.Recap_Exercise_1/lib/problems.rb
@@ -71,7 +71,13 @@ class Hash
     # hash_2.my_select { |k, v| k + 1 == v }      # => {10=>11, 5=>6, 7=>8})
     # hash_2.my_select                            # => {4=>4}
     def my_select(&prc)
-
+        new_hash = {}
+        if prc != nil
+            self.each { |k, v| new_hash[k] = v if prc.call(k, v)}
+        else
+            self.each { |k, v| new_hash[k] = v if k == v }
+        end
+        new_hash
     end
 end
 
