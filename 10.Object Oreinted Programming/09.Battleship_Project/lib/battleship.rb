72497dcbd11b7ea21138e090d6410a6a1087abfd
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-10 14:16:45 +0200

    Constructor of Battleship class

--- 10.Object Oreinted Programming/09.Battleship_Project/lib/battleship.rb
+++ 10.Object Oreinted Programming/09.Battleship_Project/lib/battleship.rb
@@ -2,5 +2,10 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
-
+    def initialize(n)
+        @player = Player.new
+        size = n * n
+        @board = Board.new(n)
+        @remaining_misses = 0.5 * size
+    end
 end
