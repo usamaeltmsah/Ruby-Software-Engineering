9b5ca797b1ffdeea8f97c60dd71c0ad62eb65365
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-10 02:56:25 +0200

    Make the constructor of the Code class

--- 10.Object Oreinted Programming/08.Mastermind_Project/lib/code.rb
+++ 10.Object Oreinted Programming/08.Mastermind_Project/lib/code.rb
@@ -10,4 +10,9 @@ class Code
     pegs_chars = POSSIBLE_PEGS.keys
     chars_arr.all? { |ch| pegs_chars.include?(ch.upcase) }
   end
+
+  def initialize(char_arr)
+    raise "Not valid pegs!" if !Code.valid_pegs?(char_arr)
+    @pegs = char_arr.map(&:upcase)
+  end
 end
