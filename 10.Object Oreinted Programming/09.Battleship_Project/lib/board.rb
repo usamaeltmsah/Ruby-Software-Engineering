d0ccfe0222668e7ae62ff64b5eecc172b55a3a07
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-10 04:20:15 +0200

    Constructor of Board class

--- 10.Object Oreinted Programming/09.Battleship_Project/lib/board.rb
+++ 10.Object Oreinted Programming/09.Battleship_Project/lib/board.rb
@@ -1,3 +1,9 @@
 class Board
-  
+  def initialize(n)
+    @grid = Array.new(n) { Array.new(n)}
+    (0...n).each do |i|
+         (0...n).each { |j| @grid[i][j] = :N }
+    end
+    @size = n * n
+  end
 end
