767aaca4e980a09f24909234f83536d4b95de4cc
Usama Fouad <usamaeltmsah@gmail.com>
2020-09-11 02:07:44 +0200

    Swap keys an values in hash

--- 12.Recap_Exercises/2.Recap_Exercise_2/lib/problems.rb
+++ 12.Recap_Exercises/2.Recap_Exercise_2/lib/problems.rb
@@ -25,7 +25,9 @@ end
 class Hash
     # Write a method, Hash#inverse, that returns a new hash where the key-value pairs are swapped
     def inverse
-
+        new_hash = {}
+        self.each { |k, v| new_hash[v] = k}
+        new_hash
     end
 end
 
