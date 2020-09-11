ab5b10afebcdd2704c75eabbfd8feedbb694600a
Usama Fouad <usamaeltmsah@gmail.com>
2020-09-11 03:02:42 +0200

    Check that there's no consecutive repeats

--- 12.Recap_Exercises/3.Recap_Exercise_3/problems.rb
+++ 12.Recap_Exercises/3.Recap_Exercise_3/problems.rb
@@ -12,3 +12,14 @@ p no_dupes?(['x', 'x', 'y', 'z', 'z'])     # => ['y']
 p no_dupes?([true, true, true])            # => []
 
 # ---------------------------------------------------------------------------
+
+def no_consecutive_repeats?(arr)
+    (0...arr.length-1).all? { |i| arr[i] != arr[i+1] }
+end
+
+# Examples
+p no_consecutive_repeats?(['cat', 'dog', 'mouse', 'dog'])     # => true
+p no_consecutive_repeats?(['cat', 'dog', 'dog', 'mouse'])     # => false
+p no_consecutive_repeats?([10, 42, 3, 7, 10, 3])              # => true
+p no_consecutive_repeats?([10, 42, 3, 3, 10, 3])              # => false
+p no_consecutive_repeats?(['x'])                              # => true
