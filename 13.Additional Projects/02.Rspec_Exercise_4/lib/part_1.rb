21b49594aba23ba64afaaddd9b416ebd485a5621
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-12 11:42:05 +0200

    Create my own one? function for arrays

--- 13.Additional Projects/02.Rspec_Exercise_4/lib/part_1.rb
+++ 13.Additional Projects/02.Rspec_Exercise_4/lib/part_1.rb
@@ -1,3 +1,10 @@
 def my_reject(arr, &prc)
     arr.select { |el| prc.call(el) == false }
 end
+
+def my_one?(arr, &prc)
+    c = 0
+    arr.each { |el| c += 1 if prc.call(el) }
+    return true if c == 1
+    return false
+end
