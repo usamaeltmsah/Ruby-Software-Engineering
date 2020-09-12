7731ae46a56709f5847a31c5199ec99d57756908
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-12 02:34:31 +0200

    Get the nth prime

--- 13.Additional Projects/01.Rspec_Exercise_3/lib/part_1.rb
+++ 13.Additional Projects/01.Rspec_Exercise_3/lib/part_1.rb
@@ -2,3 +2,17 @@ def is_prime?(num)
     return false if num < 2
     (2...num).each.all? { |i| num % i != 0  }
 end
+
+def nth_prime(n)
+    i = 1
+    j = 2
+    while i <= n
+        while !is_prime?(j)
+            j += 1
+        end
+        i += 1
+        prime = j
+        j += 1
+    end
+    prime
+end
