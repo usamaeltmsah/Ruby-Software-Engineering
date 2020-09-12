c0ad0bb0850b1f660f0ef36ed8770d3cdb85c9da
Usama Fouad <usamaeltmsah@gmail.com>
2020-09-12 12:10:40 +0200

    Is it a perfect number?

--- 13.Additional Projects/02.Rspec_Exercise_4/lib/part_2.rb
+++ 13.Additional Projects/02.Rspec_Exercise_4/lib/part_2.rb
@@ -8,3 +8,8 @@ def aliquot_sum(num)
     proper_factors = proper_factors(num)
     proper_factors.sum
 end
+
+def perfect_number?(num)
+    return true if num == aliquot_sum(num)
+    return false
+end
