4b750eb18d90f0f1c9de3d86743f045aa95545e0
Usama Fouad <usamaeltmsah@gmail.com>
2020-09-12 12:09:19 +0200

    Sum all the proper factors of a number

--- 13.Additional Projects/02.Rspec_Exercise_4/lib/part_2.rb
+++ 13.Additional Projects/02.Rspec_Exercise_4/lib/part_2.rb
@@ -3,3 +3,8 @@ def proper_factors(num)
     (1...num).each { |i| factors << i if num % i == 0 }
     factors
 end
+
+def aliquot_sum(num)
+    proper_factors = proper_factors(num)
+    proper_factors.sum
+end
