1ad0ec67ce5f3d206abe58ee2d3e7b357a177c18
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-13 21:51:11 +0200

    Pretentious primes

--- 13.Additional Projects/Nauseating_Numbers.rb
+++ 13.Additional Projects/Nauseating_Numbers.rb
@@ -400,3 +400,41 @@ p consecutive_collapse([1, 9, 8, 6, 4, 5, 7, 9, 2])   # [1, 9, 2]
 p consecutive_collapse([3, 5, 6, 2, 1])               # [1]
 p consecutive_collapse([5, 7, 9, 9])                  # [5, 7, 9, 9]
 p consecutive_collapse([13, 11, 12, 12])              # []
+
+# ---------------------------------------------------------------------------------------------------------------
+
+def pretentious_primes(arr, num)
+    primes = []
+    arr.each do |el|
+        i = 1
+        # Factor is a variable determine is the number +ve or -ve
+        factor = 1 if num >= 0
+        factor = -1 if num < 0
+            while i <= num * factor
+                el += 1 * factor
+                while !is_prime?(el)
+                    if el < 0
+                        el = nil
+                        break
+                    end
+                    el += 1 * factor
+                end
+                i += 1
+                prime = el
+                break if el == nil
+            end
+        primes << prime
+    end
+    primes
+end
+
+p pretentious_primes([4, 15, 7], 1)           # [5, 17, 11]
+p pretentious_primes([4, 15, 7], 2)           # [7, 19, 13]
+p pretentious_primes([12, 11, 14, 15, 7], 1)  # [13, 13, 17, 17, 11]
+p pretentious_primes([12, 11, 14, 15, 7], 3)  # [19, 19, 23, 23, 17]
+p pretentious_primes([4, 15, 7], -1)          # [3, 13, 5]
+p pretentious_primes([4, 15, 7], -2)          # [2, 11, 3]
+p pretentious_primes([2, 11, 21], -1)         # [nil, 7, 19]
+p pretentious_primes([32, 5, 11], -3)         # [23, nil, 3]
+p pretentious_primes([32, 5, 11], -4)         # [19, nil, 2]
+p pretentious_primes([32, 5, 11], -5)         # [17, nil, nil]
