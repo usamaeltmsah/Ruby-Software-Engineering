d40a1bc5859c10b2f7f7067540bd27817679dfd4
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-13 02:21:42 +0200

    Sum two 2D arrays

--- 13.Additional Projects/Nauseating_Numbers.rb
+++ 13.Additional Projects/Nauseating_Numbers.rb
@@ -87,3 +87,28 @@ p anti_prime?(5)    # false
 p anti_prime?(100)  # false
 p anti_prime?(136)  # false
 p anti_prime?(1024) # false
+
+# ---------------------------------------------------------------------------------------------------------------
+
+def matrix_addition(mat1, mat2)
+    row = mat1.length
+    col = mat1[0].length
+    sum_mat = Array.new(row) { Array.new(col, 0) }
+    (0...row).each do |i|
+        (0...col).each do |j|
+            sum_mat[i][j] = mat1[i][j] + mat2[i][j]
+        end
+    end
+    sum_mat
+end
+
+matrix_a = [[2,5], [4,7]]
+matrix_b = [[9,1], [3,0]]
+matrix_c = [[-1,0], [0,-1]]
+matrix_d = [[2, -5], [7, 10], [0, 1]]
+matrix_e = [[0 , 0], [12, 4], [6,  3]]
+
+p matrix_addition(matrix_a, matrix_b) # [[11, 6], [7, 7]]
+p matrix_addition(matrix_a, matrix_c) # [[1, 5], [4, 6]]
+p matrix_addition(matrix_b, matrix_c) # [[8, 1], [3, -1]]
+p matrix_addition(matrix_d, matrix_e) # [[2, -5], [19, 14], [6, 4]]
