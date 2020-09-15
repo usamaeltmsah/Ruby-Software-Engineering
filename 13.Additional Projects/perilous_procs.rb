a81706f85a08c89b1747ec7511b810637d1e2aa9
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-15 05:56:31 +0200

    Make exactly? function to check if proc code satisfay for exactly n elements of the array

--- 13.Additional Projects/perilous_procs.rb
+++ 13.Additional Projects/perilous_procs.rb
@@ -12,3 +12,19 @@ p some?(['squash', 'corn', 'kale', 'potato']) { |str| str[0] == 'p' } # true
 p some?(['parsnip', 'lettuce', 'pea']) { |str| str[0] == 'p' }        # true
 
 # ----------------------------------------------------------------------------
+
+def exactly?(arr, n, &prc)
+    c = 0
+    arr.each { |el| c += 1 if prc.call(el) }
+    return c == n
+end
+
+p exactly?(['A', 'b', 'C'], 2) { |el| el == el.upcase }         # true
+p exactly?(['A', 'B', 'C'], 2) { |el| el == el.upcase }         # false
+p exactly?(['A', 'B', 'C'], 3) { |el| el == el.upcase }         # true
+p exactly?(['cat', 'DOG', 'bird'], 1) { |el| el == el.upcase }  # true
+p exactly?(['cat', 'DOG', 'bird'], 0) { |el| el == el.upcase }  # false
+p exactly?(['cat', 'dog', 'bird'], 0) { |el| el == el.upcase }  # true
+p exactly?([4, 5], 3) { |n| n > 0 }                             # false
+p exactly?([4, 5, 2], 3) { |n| n > 0 }                          # true
+p exactly?([42, -9, 7, -3, -6], 2) { |n| n > 0 }                # true
