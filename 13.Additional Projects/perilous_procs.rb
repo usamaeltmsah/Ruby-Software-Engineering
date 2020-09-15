fbaf48dd30dd9de0c2367c433b8276aba57179b8
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-15 14:30:13 +0200

    Procipher

--- 13.Additional Projects/perilous_procs.rb
+++ 13.Additional Projects/perilous_procs.rb
@@ -305,3 +305,50 @@ p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], is_upcas
 
 p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], begins_w, is_upcase, is_yelled, contains_a)
 # {1=>["WHO", "what", "when!", "WHERE!", "WHY"], 2=>[], 3=>[], 4=>[]}
+
+
+# ----------------------------------------------------------------------------
+
+def procipher(sent, hash)
+    words = sent.split
+    words.each_with_index { |w, i|
+        trues = []
+        hash.each { |k, v| trues << k if k.call(w) }
+        trues.each { |k| w = words[i] = hash[k].call(w) }
+    }
+    words.join(" ")
+end
+
+is_yelled = Proc.new { |s| s[-1] == '!' }
+is_upcase = Proc.new { |s| s.upcase == s }
+contains_a = Proc.new { |s| s.downcase.include?('a') }
+make_question = Proc.new { |s| s + '???' }
+reverse = Proc.new { |s| s.reverse }
+add_smile = Proc.new { |s| s + ':)' }
+
+p procipher('he said what!',
+    is_yelled => make_question,
+    contains_a => reverse
+) # "he dias ???!tahw"
+
+p procipher('he said what!',
+    contains_a => reverse,
+    is_yelled => make_question
+) # "he dias !tahw???"
+
+p procipher('he said what!',
+    contains_a => reverse,
+    is_yelled => add_smile
+) # "he dias !tahw:)"
+
+p procipher('stop that taxi now',
+    is_upcase => add_smile,
+    is_yelled => reverse,
+    contains_a => make_question
+) # "stop that??? taxi??? now"
+
+p procipher('STOP that taxi now!',
+    is_upcase => add_smile,
+    is_yelled => reverse,
+    contains_a => make_question
+) # "STOP:) that??? taxi??? !won"
