1c81bd3a4017591acdd028252250b6127e17957c
Usama Fouad <usamaeltmsah@gmail.com>
2020-09-15 12:56:15 +0200

    Selected map!

--- 13.Additional Projects/perilous_procs.rb
+++ 13.Additional Projects/perilous_procs.rb
@@ -188,3 +188,32 @@ end
 p proctition([4, -5, 7, -10, -2, 1, 3]) { |el| el > 0 } # [4, 7, 1, 3, -5, -10, -2]
 p proctition([7, 8, 3, 6, 10]) { |el| el.even? } # [8, 6, 10, 7, 3]
 p proctition(['cat','boot', 'dog', 'bug', 'boat']) { |s| s[0] == 'b' } # ["boot", "bug", "boat", "cat", "dog"]
+
+# ----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+
+### Phase 3: Perilous.
+
+def selected_map!(arr, prc1, prc2)
+    arr.each_with_index do |el, i|
+        arr[i] = prc2.call(el) if prc1.call(el)
+    end
+    nil
+end
+
+is_even = Proc.new { |n| n.even? }
+is_positive = Proc.new { |n| n > 0 }
+square = Proc.new { |n| n * n }
+flip_sign = Proc.new { |n| -n }
+
+arr_1 = [8, 5, 10, 4]
+p selected_map!(arr_1, is_even, square)     # nil
+p arr_1                                     # [64, 5, 100, 16]
+
+arr_2 = [-10, 4, 7, 6, -2, -9]
+p selected_map!(arr_2, is_even, flip_sign)  # nil
+p arr_2                                     # [10, -4, 7, -6, 2, -9]
+
+arr_3 = [-10, 4, 7, 6, -2, -9]
+p selected_map!(arr_3, is_positive, square) # nil
+p arr_3                                     # [-10, 16, 49, 36, -2, -9]
