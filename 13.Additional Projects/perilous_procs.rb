66e11bdc4cc44460949c33b2911c3a965977d5f8
Usama Fouad <usamaeltmsah@gmail.com>
2020-09-15 14:40:48 +0200

    Picky procipher

--- 13.Additional Projects/perilous_procs.rb
+++ 13.Additional Projects/perilous_procs.rb
@@ -352,3 +352,52 @@ p procipher('STOP that taxi now!',
     is_yelled => reverse,
     contains_a => make_question
 ) # "STOP:) that??? taxi??? !won"
+
+# ----------------------------------------------------------------------------
+
+def picky_procipher(sent, hash)
+    words = sent.split
+    words.each_with_index { |w, i|
+        hash.each { |k, v|
+            if k.call(w)
+                words[i] = v.call(w)
+                break
+            end
+        }
+    }
+    words.join(" ")
+end
+
+is_yelled = Proc.new { |s| s[-1] == '!' }
+is_upcase = Proc.new { |s| s.upcase == s }
+contains_a = Proc.new { |s| s.downcase.include?('a') }
+make_question = Proc.new { |s| s + '???' }
+reverse = Proc.new { |s| s.reverse }
+add_smile = Proc.new { |s| s + ':)' }
+
+p picky_procipher('he said what!',
+    is_yelled => make_question,
+    contains_a => reverse
+) # "he dias what!???"
+
+p picky_procipher('he said what!',
+    contains_a => reverse,
+    is_yelled => make_question
+) # "he dias !tahw"
+
+p picky_procipher('he said what!',
+    contains_a => reverse,
+    is_yelled => add_smile
+) # "he dias !tahw"
+
+p picky_procipher('stop that taxi now',
+    is_upcase => add_smile,
+    is_yelled => reverse,
+    contains_a => make_question
+) # "stop that??? taxi??? now"
+
+p picky_procipher('STOP that taxi!',
+    is_upcase => add_smile,
+    is_yelled => reverse,
+    contains_a => make_question
+) # "STOP:) that??? !ixat"
