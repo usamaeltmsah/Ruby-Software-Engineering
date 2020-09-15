8dc0c5eef4b751f3b84c0b8814428cf619917dfa
Usama Fouad <usamaeltmsah@gmail.com>
2020-09-15 11:19:50 +0200

    Are all array's element true with the given condition

--- 13.Additional Projects/perilous_procs.rb
+++ 13.Additional Projects/perilous_procs.rb
@@ -61,3 +61,17 @@ p at_least?([false, false, false], 3) { |bool| bool } # false
 p at_least?([false, true, true], 3) { |bool| bool } # false
 p at_least?([true, true, true], 3) { |bool| bool } # true
 p at_least?([true, true, true, true], 3) { |bool| bool } # true
+
+# ----------------------------------------------------------------------------
+
+def every?(arr, &prc)
+    arr.each { |el| return false if !prc.call(el) }
+    return true
+end
+
+p every?([3, 1, 11, 5]) { |n| n.even? }                                 # false
+p every?([2, 4, 4, 8]) { |n| n.even? }                                  # true
+p every?([8, 2]) { |n| n.even? }                                        # true
+p every?(['squash', 'corn', 'kale', 'carrot']) { |str| str[0] == 'p' }  # false
+p every?(['squash', 'pea', 'kale', 'potato']) { |str| str[0] == 'p' }   # false
+p every?(['parsnip', 'potato', 'pea']) { |str| str[0] == 'p' }          # true
