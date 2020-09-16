ab0167f6a3256515528b3621b57abd5b84f14a46
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-16 15:09:09 +0200

    Sentence swap words

--- 13.Additional Projects/meddling_medley.rb
+++ 13.Additional Projects/meddling_medley.rb
@@ -12,3 +12,23 @@ p duos('wxxyzz')        # 2
 p duos('hoooraay')      # 3
 p duos('dinosaurs')     # 0
 p duos('e')             # 0
+
+# ---------------------------------------------------------------------------------
+
+def sentence_swap(sent, hash)
+    words = sent.split
+    words.each_with_index { |w, i| words[i] = hash[w] if hash[w] }
+    words.join(" ")
+end
+
+p sentence_swap('anything you can do I can do',
+    'anything'=>'nothing', 'do'=>'drink', 'can'=>'shall'
+) # 'nothing you shall drink I shall drink'
+
+p sentence_swap('what a sad ad',
+    'cat'=>'dog', 'sad'=>'happy', 'what'=>'make'
+) # 'make a happy ad'
+
+p sentence_swap('keep coding okay',
+    'coding'=>'running', 'kay'=>'pen'
+) # 'keep running okay'
