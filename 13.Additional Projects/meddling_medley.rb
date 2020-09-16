7d34067be5b55376f43a5451365071ef55859ed9
Usama Fouad <usamaeltmsah@gmail.com>
2020-09-16 15:14:34 +0200

    Hash mapped

--- 13.Additional Projects/meddling_medley.rb
+++ 13.Additional Projects/meddling_medley.rb
@@ -32,3 +32,19 @@ p sentence_swap('what a sad ad',
 p sentence_swap('keep coding okay',
     'coding'=>'running', 'kay'=>'pen'
 ) # 'keep running okay'
+
+# ---------------------------------------------------------------------------------
+
+def hash_mapped(hash, prc, &block)
+    new_hash = {}
+    hash.each { |k, v| new_hash[block.call(k)] = prc.call(v) }
+    new_hash
+end
+
+double = Proc.new { |n| n * 2 }
+p hash_mapped({'a'=>4, 'x'=>7, 'c'=>-3}, double) { |k| k.upcase + '!!' }
+# {"A!!"=>8, "X!!"=>14, "C!!"=>-6}
+
+first = Proc.new { |a| a[0] }
+p hash_mapped({-5=>['q', 'r', 's'], 6=>['w', 'x']}, first) { |n| n * n }
+# {25=>"q", 36=>"w"}
