ed35c7035dd52a4b8621df7497aef6a0879246ef
Usama Fouad <usamaeltmsah@gmail.com>
2020-09-16 23:53:05 +0200

    Add flight

--- 13.Additional Projects/04.Flight_Project/lib/passenger.rb
+++ 13.Additional Projects/04.Flight_Project/lib/passenger.rb
@@ -9,4 +9,8 @@ class Passenger
     def has_flight?(flight_number)
         return @flight_numbers.any? { |fli_num| fli_num.casecmp(flight_number) == 0 }
     end
+
+    def add_flight(flight_number)
+        @flight_numbers << flight_number.upcase if !has_flight?(flight_number)
+    end
 end
