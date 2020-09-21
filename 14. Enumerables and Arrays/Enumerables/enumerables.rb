ce9a72044e24fb0b17ca466666f6496c7d55b55f
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-21 15:10:38 +0200

    Create my_zip method in Array class

--- 14. Enumerables and Arrays/Enumerables/enumerables.rb
+++ 14. Enumerables and Arrays/Enumerables/enumerables.rb
@@ -50,6 +50,18 @@ class Array
         end
         arr
     end
+
+    def my_zip(*arrs)
+        zipped = []
+        (0...self.length).each do |i|
+            tmp_arr = [self[i]]
+            arrs.my_each do |arr|
+                tmp_arr << arr[i]
+            end
+            zipped << tmp_arr
+        end
+        zipped
+    end
 end
 
 return_value = [1, 2, 3].my_each do |num|
@@ -83,3 +95,15 @@ p a.my_all? { |num| num < 4 } # => true
 # ------------------------------------------------------------------
 
 p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+
+# ------------------------------------------------------------------
+
+a = [ 4, 5, 6 ]
+b = [ 7, 8, 9 ]
+p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+
+c = [10, 11, 12]
+d = [13, 14, 15]
+p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
