3115598056053c435b37c4037828613d4efbcbc6
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-21 14:34:31 +0200

    Create my_any? and my_all? methods in Array class

--- 14. Enumerables and Arrays/Enumerables/enumerables.rb
+++ 14. Enumerables and Arrays/Enumerables/enumerables.rb
@@ -24,6 +24,20 @@ class Array
         end
         arr
     end
+
+    def my_any?(&prc)
+        self.my_each do |num|
+            return true if prc.call(num)
+        end
+        false
+    end
+
+    def my_all?(&prc)
+        self.my_each do |num|
+            return false if !prc.call(num)
+        end
+        true
+    end
 end
 
 return_value = [1, 2, 3].my_each do |num|
@@ -45,3 +59,11 @@ p a.my_select { |num| num == 4 } # => []
 a = [1, 2, 3]
 p a.my_reject { |num| num > 1 } # => [1]
 p a.my_reject { |num| num == 4 } # => [1, 2, 3]
+
+# ------------------------------------------------------------------
+
+a = [1, 2, 3]
+p a.my_any? { |num| num > 1 } # => true
+p a.my_any? { |num| num == 4 } # => false
+p a.my_all? { |num| num > 1 } # => false
+p a.my_all? { |num| num < 4 } # => true
