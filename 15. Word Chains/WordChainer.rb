cb368df7e7a3b7158cf72147beb527369ba8f8bd
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-27 00:35:56 +0200

    Read dictionary from file

--- 15. Word Chains/WordChainer.rb
+++ 15. Word Chains/WordChainer.rb
@@ -5,4 +5,8 @@ class WordChainer
     def initialize(dictionary_file_name="dictionary.txt")
         @dictionary = read_file_to_arr(dictionary_file_name)
     end
+
+    def read_file_to_arr(file)
+        Set.new(File.read(file).split)
+    end
 end
