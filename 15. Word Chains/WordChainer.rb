ed28437dd9e9818abf049cabee4d7bcf5e01bb53
Usama Fouad <usamaeltmsah@gmail.com>
2020-09-27 00:36:35 +0200

    Get all words near to this word

--- 15. Word Chains/WordChainer.rb
+++ 15. Word Chains/WordChainer.rb
@@ -9,4 +9,20 @@ class WordChainer
     def read_file_to_arr(file)
         Set.new(File.read(file).split)
     end
+
+    def adjacent_words(word)
+        adj_words = []
+
+        word.each_char.with_index do |ch, i|
+            ('a'..'z').each do |new_ch|
+                next if ch == new_ch
+
+                new_word = word.dup
+                new_word[i] = new_ch
+
+                adj_words << new_word if dictionary.include?(new_word)
+            end
+        end
+        adj_words
+    end
 end
