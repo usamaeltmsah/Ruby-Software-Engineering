9fe856ac6d3c770d4e5cb781cd287fe7a89e228b
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-30 21:59:14 +0200

    Assign parent to current node

--- 17. PolyTree/lib/00_tree_node.rb
+++ 17. PolyTree/lib/00_tree_node.rb
@@ -5,4 +5,15 @@ class PolyTreeNode
         @parent = nil
         @children = []
     end
+
+    def inspect
+        @value.inspect
+    end
+
+    def parent=(parent)
+        @parent.children.delete(self) if !@parent.nil?
+        @parent = parent
+        return nil if @parent.nil?
+        @parent.children << self if !@parent.children.include?(self)
+    end
 end
