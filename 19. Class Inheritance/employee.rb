742f27932db925048ff5dcc075a547464cf17bba
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-11 16:07:17 +0200

    Add employees to a boss

--- 19. Class Inheritance/employee.rb
+++ 19. Class Inheritance/employee.rb
@@ -1,10 +1,17 @@
 class Employee
-    attr_reader :name, :title, :salary, :boss
+    attr_reader :name, :title, :salary
+    attr_accessor :boss
     def initialize(name, title, salary, boss=nil)
         @name = name
         @title = title
         @salary = salary
         @boss = boss
+    end
+
+    def boss=(boss)
+        @boss = boss
+        boss.add_employee(self) unless boss.nil?
+        boss
     end
 
     def bonus(multiplier)
