e427d2d05fa757ad772bc42ea13d6fdaa42f7e03
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-11 15:58:03 +0200

    Calculate bonus of the manager

--- 19. Class Inheritance/manager.rb
+++ 19. Class Inheritance/manager.rb
@@ -1,7 +1,19 @@
 require_relative 'employee'
 
 class Manager < Employee
-    def initialize(employees)
+    attr_reader :employees
+    def initialize(name, title, salary, employees)
         @employees = employees
+        super(name, title, salary)
+    end
+
+    def sum_salary
+        sum = 0
+        employees.each { |employee| sum += employee.salary }
+        sum
+    end
+
+    def bonus(multiplier)
+       sum_salary * multiplier
     end
 end
