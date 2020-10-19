00caae1e6fb76e442ac45dcbec569146ac436fd7
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-19 22:40:29 +0200

    Write th two_sum method

--- 22. First TDD Project/lib/functions.rb
+++ 22. First TDD Project/lib/functions.rb
@@ -2,4 +2,18 @@ class Array
     def my_uniq
         Set.new(self).to_a
     end
+
+    def two_sum
+        pairs = []
+        len = self.length
+        (0...len).each do |i|
+            (i+1...len).each do |j|
+                if self[i] == -self[j]
+                    temp_pair = [i, j]
+                    pairs << temp_pair
+                end
+            end
+        end
+        pairs
+    end
 end
