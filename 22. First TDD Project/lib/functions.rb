311d06edc9d6aef884eeac058c6c438f5f36a18b
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-19 22:04:19 +0200

    Fix that it shouldn't take a parameter

--- 22. First TDD Project/lib/functions.rb
+++ 22. First TDD Project/lib/functions.rb
@@ -1,2 +1,5 @@
 class Array
+    def my_uniq
+        Set.new(self).to_a
+    end
 end
--- 22. First TDD Project/spec/functions_spec.rb
+++ 22. First TDD Project/spec/functions_spec.rb
@@ -5,7 +5,7 @@ describe Array do
     
     describe "#my_uniq" do
         it "Shouldn't accept parameters" do
-            expect { [1, 4, 6] }.to_not raise_error
+            expect { [1, 4, 6].my_uniq }.to_not raise_error
         end
         it "Not allowed use uniq method" do
             expect_any_instance_of(Array).to_not receive(:uniq)
