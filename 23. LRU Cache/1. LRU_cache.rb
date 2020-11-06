d370b45bc7d0a40ca2aa4d6532fad6969df28566
usamaeltmsah <usamaeltmsah@gmail.com>
2020-11-06 06:48:30 +0200

    Get the count of elements currently in cache

--- 23. LRU Cache/1. LRU_cache.rb
+++ 23. LRU Cache/1. LRU_cache.rb
@@ -1,9 +1,11 @@
 class LRUCache
     def initialize
+      @cache = []
     end
 
     def count
       # returns number of elements currently in cache
+      cache.length
     end
 
     def add(el)
